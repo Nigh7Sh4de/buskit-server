6db4e12d0ca32e2879809f8d7f4204932ecd0316
Dennis Plotnik <plotnikd@gmail.com>
2018-05-20 05:00:13 -0400

    Can receieve twitch auth code from mobile

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,19 @@
 const Router = require('koa-router')
 const Passport = require('koa-passport')
+const axios = require('axios')
 
 module.exports = class Auth extends Router {
   constructor(app) {
     super()
     this.get('/auth/twitch/', Passport.authenticate('twitch', { forceVerify: true }))
-    this.get('/auth/twitch/redirect', Passport.authenticate('twitch'), ctx => {
-      console.log('You made it!')
-      ctx.body = "You made it!"
+    this.get('/auth/twitch/redirect', async ctx => {
+      try {
+        const result = await axios.post('https://id.twitch.tv/oauth2/token?client_id=k6zpqqplgc8nyknrnkag6qhfpesc9p&client_secret=lhkndwvkvhqgqx6yv1rulqqcpc02am&code=' + ctx.request.query.code + '&grant_type=authorization_code&redirect_uri=buskit://buskit.tv/redirect')
+        return ctx.body = result.data
+      }
+      catch(err) {
+        return ctx.throw(400, err)
+      }
     })
   }
 }
