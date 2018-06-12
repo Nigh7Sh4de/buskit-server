db40cd323b92394439ae3d097ce7a5b904132572
Dennis Plotnik <plotnikd@gmail.com>
2018-06-12 00:02:33 -0400

    Add streams endpoint

---
+++ src/routes/streams.js
@@ -0,0 +1,19 @@
+const Router = require('koa-router')
+const axios = require('axios')
+
+module.exports = class Users extends Router {
+  constructor(app) {
+    super()
+
+    this.get('/streams', app.passport.authenticate('jwt', { session: false}), async ctx => {
+      const streams = await axios.get('https://api.twitch.tv/helix/streams', {
+        headers: {
+          'Client-ID': 'k6zpqqplgc8nyknrnkag6qhfpesc9p'
+        }
+      })
+      return ctx.body = {
+        data: streams.data
+      }
+    })
+  }
+}
