149d469f059b01b551a42e0773906c186a4078be
Dennis Plotnik <plotnikd@gmail.com>
2018-06-18 22:05:59 -0400

    Refactor get user to fetch from twitch when necessary

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,25 @@
 const Router = require('koa-router')
+const axios = require('axios')
 
 
 module.exports = class Users extends Router {
   constructor(app) {
     super()
 
-    this.get('/users/:id', app.passport.authenticate('jwt', { session: false}), async ctx => {
-      return ctx.body = ctx.req.user
+    this.get('/users/:id', app.passport.authenticate('jwt', { session: false}), this.getUser)
+  }
+
+  async getUser(ctx) {
+    if (ctx.req.user.id === ctx.params.id) return ctx.body = ctx.req.user
+    
+    const response = await axios.get(`https://api.twitch.tv/helix/users?id=${ctx.params.id}`, {
+      headers: {
+        'Client-ID': 'k6zpqqplgc8nyknrnkag6qhfpesc9p'
+      }
     })
+
+    return ctx.body = {
+      data: response.data.data[0] || null
+    }
   }
 }
