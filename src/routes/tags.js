f1c67f62673489c2574fba776bc70418f33892b7
thewa <thewa@scs.ryerson.ca>
2018-07-03 23:43:38 -0400

    added some tag functionality

--- src/routes/tags.js
+++ src/routes/tags.js
@@ -1,12 +1,13 @@
 const Router = require("koa-router");
-const router = new Router();
+const Tags = require("../db/tags");
 
 module.exports = class Tags extends Router {
     constructor(app){
-        super()
-        this.get('/tags', { session: false}, this.getTags)
+        super();
+        this.get("/tags", this.getTags);
     }
     async getTags(ctx){
-        
+        const tags = await Tags.find({});
+        ctx.body = tags;
     }
 }
