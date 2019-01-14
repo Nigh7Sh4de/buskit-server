1dc8a4b9aa4d5e5bee3e22431718f723f4f26749
nigh7sh4de <plotnikd@gmail.com>
2019-01-14 17:23:59 -0500

    Make GET streams a public API

---
+++ src/routes/streams.js
@@ -0,0 +1,44 @@
+const Router = require('koa-router')
+const axios = require('axios')
+
+module.exports = class Users extends Router {
+  constructor(app) {
+    super()
+    this.app = app
+
+    this.get('/streams', this.getStreams.bind(this))
+    this.get('/streams/sub', this.verifyStreamSub.bind(this))
+    this.post('/streams/sub', this.processStreamSub.bind(this))
+  }
+
+  async getStreams(ctx) {
+    const streams = await this.db.streams.find({}).exec()
+    return ctx.body = {
+      streams,
+    }
+  }
+
+  async verifyStreamSub(ctx) {
+    ctx.body = ctx.request.query['hub.challenge']
+  }
+
+  async processStreamSub(ctx) {
+    const { query, body } = ctx.request
+    const { user_id } = query
+    const { data } = body
+
+    ctx.status = 200
+    
+    let user = null
+    try {
+      user = await this.app.db.users.findById(user_id).exec()
+      if (!user) throw new Error(`User not found with id ${user_id}`)
+    }
+    catch(e) {
+      ctx.throw(404, e)
+    }
+
+    user.streams = data
+    user.save()
+  }
+}
